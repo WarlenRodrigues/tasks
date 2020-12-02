320f5fa53ff6c98737c43157f78066a0f13ccd8d
Warlen Rodrigues <warlen.cesarrodrigues@gmail.com>
2020-12-02 06:11:25 -0300

    [FIX] Fixed db ip

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'tasks',
         'USER': 'cloud',
         'PASSWORD': 'cloud',
-        'HOST': '52.15.77.89',
+        'HOST': 'node1',
         'PORT': '5432',
     }
 }
